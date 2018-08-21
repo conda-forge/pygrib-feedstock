ae59d27c3bf02cff3f58955a12e3b3bfbe8f997b
Filipe Fernandes <ocefpaf@gmail.com>
2018-08-20 18:03:03 -0700

    remove openjpeg

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,5 @@
 export GRIBAPI_DIR=$PREFIX
 export JASPER_DIR=$PREFIX
 export PNG_DIR=$PREFIX
-export OPENJPEG_DIR=$PREFIX
 export ZLIB_DIR=$PREFIX
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
