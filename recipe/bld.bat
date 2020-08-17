6ca07d753e82059e56eaf1604df90ff13344719a
Filipe <ocefpaf@gmail.com>
2020-08-17 13:54:56 -0300

    Update recipe/bld.bat
    Co-authored-by: Isuru Fernando <isuruf@gmail.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,8 +4,7 @@ SET PNG_DIR=%PREFIX%
 SET ZLIB_DIR=%PREFIX%
 
 :: Replace the static library with shared
-del %LIBRARY_LIB%\libpng16_static.lib
-copy %LIBRARY_LIB%\libpng16.lib %LIBRARY_LIB%\libpng16_static.lib
+copy %LIBRARY_LIB%\libpng16.lib %LIBRARY_LIB%\png.lib
 del %LIBRARY_LIB%\zlibstatic.lib
 copy %LIBRARY_LIB%\z.lib %LIBRARY_LIB%\zlibstatic.lib
 
