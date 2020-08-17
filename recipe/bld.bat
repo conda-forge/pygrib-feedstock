3c41ce4918c6dcc54dc9430a45249a3ddc913a36
Filipe Fernandes <ocefpaf@gmail.com>
2020-08-17 13:39:00 -0300

    copy shared to static name

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,4 +2,11 @@ SET GRIBAPI_DIR=%PREFIX%
 SET ZLIB_DIR=%PREFIX%
 SET PNG_DIR=%PREFIX%
 SET ZLIB_DIR=%PREFIX%
+
+:: Replace the static library with shared
+del %LIBRARY_LIB%\libpng16_static.lib
+copy %LIBRARY_LIB%\libpng16.lib %LIBRARY_LIB%\libpng16_static.lib
+del %LIBRARY_LIB%\zlibstatic.lib
+copy %LIBRARY_LIB%\z.lib %LIBRARY_LIB%\zlibstatic.lib
+
 %PYTHON% -m pip install . --no-deps -vv
